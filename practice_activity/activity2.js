5a46510b0cd7fadcf593699a9d776bcc0dba6eca
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-08-30 11:27:31 -0500

    Update activity2.js

--- practice_activity/activity2.js
+++ practice_activity/activity2.js
@@ -6,5 +6,5 @@ window.location = "activity_1.html";
 function getScore()
 {
 	score = localStorage.getItem("score");	
-	document.getElementById("update").innerHTML = "<h1>Score: " + score +"</h1>";
+	document.getElementById("update").innerHTML = "<h1>Puntuación: " + score +"</h1>";
 }
